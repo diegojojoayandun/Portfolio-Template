533d71a40875b430e41f7adaac946f4b28d32baf
Diego Fernando Jojoa <91098381+diegojojoayandun@users.noreply.github.com>
2024-09-06 16:06:39 -0500

    Update: improve responsive for footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,28 +3,31 @@ import React from "react";
 const Footer = () => {
   return (
     <footer id="footer">
-      <div className="container">
+      <div className="container flex flex-col items-center justify-center">
         <p></p>
-        <div className="social-links">
+        <div className="social-links flex justify-center items-center gap-4 mt-4">
           <a href="https://x.com/Xcryp70R___" className="twitter">
-            <i className="bx bxl-twitter bx-md"></i>
+            <i className="bx bxl-twitter text-xl xs:text-3xl"></i>
           </a>
           <a
             href="https://www.linkedin.com/in/diego-fernando-jojoa-yandun/"
             className="linkedin"
           >
-            <i className="bx bxl-linkedin bx-md"></i>
+            <i className="bx bxl-linkedin text-xl  xs:text-3xl"></i>
           </a>
-          <a href="https://x.com/Xcryp70R___" className="twitter">
-            <i className="bx bxl-github bx-md"></i>
+          <a href="https://github.com/diegojojoayandun" className="github">
+            <i className="bx bxl-github text-xl xs:text-3xl"></i>
           </a>
-          <a href="https://x.com/Xcryp70R___" className="twitter">
-            <i className="bx bxl-gitlab bx-md"></i>
+          <a href="https://gitlab.com/xcryptorlabs" className="gitlab">
+            <i className="bx bxl-gitlab text-xl  xs:text-3xl"></i>
           </a>
         </div>
-        <div className="copyright"></div>
-        <div className="credits font-beckman sm:text-[20px] text-[60px]">
-          Designed by <a href="#">Diego Fernando Jojoa</a>
+        <div className="copyright mt-4"></div>
+        <div className="credits font-beckman sm:text-[20px] text-[60px] mt-4">
+          <span className="designed-text">Design by</span>{" "}
+          <a className="designed-name font-beckman text-[16px]" href="#">
+            Diego Fernando Jojoa
+          </a>
         </div>
       </div>
     </footer>
